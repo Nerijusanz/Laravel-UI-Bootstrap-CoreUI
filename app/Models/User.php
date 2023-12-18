e846dc3c4cceb25bcbe86b854f1f5a96ce825fed
Nerijusanz <nerijusanz@gmail.com>
2023-12-18 16:11:03 +0200

    roles&permissions models,tables,seeds

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,40 +6,54 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Laravel\Sanctum\HasApiTokens;
+
+use Carbon\Carbon;
+use Hash;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public $table = 'users';
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'created_at',
+        'updated_at',
+        'deleted_at',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function setPasswordAttribute($input)
+    {
+        if ($input) {
+            $this->attributes['password'] = app('hash')->needsRehash($input) ? Hash::make($input) : $input;
+        }
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
---
+++ database/seeders/PermissionsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Permission;
+
+class PermissionsTableSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+
+        $i                = 1;
+        $permissions      = [];
+        $permissionGroups = [
+            'permission', 'role', 'user'
+        ];
+
+        foreach ($permissionGroups as $permissionGroup) {
+            foreach (['access', 'create', 'edit', 'show', 'delete'] as $permission) {
+                $permissions[] = [
+                    'id'    => $i++,
+                    'title' => $permissionGroup . '_management_' . $permission,
+                    'created_at' => now()
+                ];
+            }
+        }
+
+        Permission::insert($permissions);
+
+    }
+}
