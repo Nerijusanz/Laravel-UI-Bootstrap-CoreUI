d7c0aa6779236564564f2c2bcad2972b055bbe72
Nerijusanz <nerijusanz@gmail.com>
2023-12-22 12:39:21 +0200

    forms erros field fix

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -19,52 +19,52 @@
                         @csrf
                         <div class="form-group">
                             <label class="required" for="name">{{ __('cruds.user.fields.name') }}</label>
-                            <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
-                            @if($errors->has('name'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('name') }}
-                                </div>
-                            @endif
+                            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="required" for="email">{{ __('cruds.user.fields.email') }}</label>
-                            <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" id="email" value="{{ old('email') }}" required>
-                            @if($errors->has('email'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('email') }}
-                                </div>
-                            @endif
+                            <input class="form-control @error('email') is-invalid @enderror" type="email" name="email" id="email" value="{{ old('email') }}" required>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="required" for="password">{{ __('cruds.user.fields.password') }}</label>
-                            <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password" required>
-                            @if($errors->has('password'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('password') }}
-                                </div>
-                            @endif
+                            <input class="form-control @error('password') is-invalid @enderror" type="password" name="password" id="password" required>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="required" for="password_confirmation">{{ __('cruds.user.fields.password_confirmation') }}</label>
-                            <input class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}" type="password" name="password_confirmation" id="password_confirmation" required>
-                            @if($errors->has('password_confirmation'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('password_confirmation') }}
-                                </div>
-                            @endif
+                            <input class="form-control @error('password_confirmation') is-invalid @enderror" type="password" name="password_confirmation" id="password_confirmation" required>
+                            @error('password_confirmation')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="required" for="role">{{ __('cruds.user.fields.role') }}</label>
-                            <select class="form-control select2 {{ $errors->has('role') ? 'is-invalid' : '' }}" name="role" id="role" required>
+                            <select class="form-control select2 @error('role') is-invalid @enderror" name="role" id="role" required>
                                 @foreach($roles as $role)
                                     <option value="{{ $role->id }}" {{ old('role', 2) == $role->id ? 'selected' : '' }}>{{ $role->title }}</option>
                                 @endforeach
                             </select>
-                            @if($errors->has('role'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('role') }}
-                                </div>
-                            @endif
+                            @error('role')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group mt-2">
                             <button class="btn btn-sm btn-success" type="submit">
