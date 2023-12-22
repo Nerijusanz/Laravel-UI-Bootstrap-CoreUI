7fb2303d0c743ee043cc6aacc6bca904487d3352
Nerijusanz <nerijusanz@gmail.com>
2023-12-22 11:11:50 +0200

    fix migrations fk, idx

--- database/migrations/2023_12_18_124609_permission_role_table.php
+++ database/migrations/2023_12_18_124609_permission_role_table.php
@@ -12,8 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permission_role', function (Blueprint $table) {
-            $table->foreignId('role_id')->references('id')->on('roles')->cascadeOnDelete();
-            $table->foreignId('permission_id')->references('id')->on('permissions')->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('permission_id')->constrained()->cascadeOnDelete();
+
+            $table->index('role_id','permission_role_role_id_idx');
+            $table->index('permission_id','permission_role_permission_id_idx');
         });
     }
 
--- database/migrations/2023_12_18_125623_create_role_user_table.php
+++ database/migrations/2023_12_18_125623_create_role_user_table.php
@@ -10,8 +10,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreignId('role_id')->references('id')->on('roles')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+
+            $table->index('user_id','role_user_user_id_idx');
+            $table->index('role_id','role_user_role_id_idx');
         });
     }
 
