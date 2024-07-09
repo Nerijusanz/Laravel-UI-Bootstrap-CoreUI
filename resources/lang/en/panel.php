d4d06c20b94c2aadeae29235734f0b2754f4c190
Nerijusanz <nerijusanz@gmail.com>
2024-07-09 14:49:00 +0300

    109_create_sidabar_translations closes #109

--- resources/lang/en/panel.php
+++ resources/lang/en/panel.php
@@ -2,4 +2,17 @@
 
 return [
     'site_title' => 'SITE TITLE [EN]',
+    'sidebar' => [
+        'dashboard' => 'Dashboard',
+        'groups' => [
+            'user_management' => [
+                'title' => 'User management',
+                'nav_links' => [
+                    'users' => 'Users',
+                    'roles' => 'Roles',
+                    'permissions' => 'Permissions'
+                ],
+            ],
+        ],
+    ]
 ];
